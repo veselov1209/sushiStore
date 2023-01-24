ef1ceaccbf22ddb065bb00611adb90be1d9958de
veselov1209 <veselov1209@gmail.com>
2023-01-24 14:41:52 +0200

    added menu

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,13 @@
+import MealList from "./MealList";
+import PromoText from "./PromoText";
+
+const Meals = () => {
+  return (
+    <>
+      <PromoText />
+      <MealList />
+    </>
+  );
+};
+
+export default Meals;
---
+++ src/components/Meals/PromoText.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import style from "./PromoText.module.css";
+
+function PromoText() {
+  return (
+    <section className={style["promo-text"]}>
+      <h2>Lorem ipsum dolor sit amet.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        blanditiis repellat natus accusamus. Reiciendis, obcaecati quo quam quis
+        ullam vero eos dolore.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        blanditiis repellat natus accusamus. Reiciendis, obcaecati quo quam quis
+        ullam vero eos dolore.
+      </p>
+    </section>
+  );
+}
+
+export default PromoText;
